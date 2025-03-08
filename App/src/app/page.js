22f03deba3b61c4cc248fa9a3310dcfd7cec0062
Seth <142448427+Flucoreo@users.noreply.github.com>
2025-03-08 07:29:45 -0800

    starting front end

---
+++ App/src/app/page.js
@@ -0,0 +1,13 @@
+import "./globals.css"
+
+export default function Home() {
+  return (
+    <>
+      <div className="bg-black text-white h-screen">
+
+        <h1 className="text-center text-4xl font-bold py-10">Charity Growth Predictor</h1>
+
+      </div>
+    </>
+  );
+}
